cb2e66edd726624992fcacd4329466582a6ca8ed
Cyrus Roshan <cyrusroshan123@gmail.com>
2019-02-03 23:46:48 -0800

    Get basic idea for the main screen.

--- App.tsx
+++ App.tsx
@@ -8,8 +8,11 @@
  */
 
 import React from 'react';
-import {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import { Component } from 'react';
+import { Platform, StyleSheet, Text, ScrollView, View } from 'react-native';
+import { iOSUIKit } from 'react-native-typography';
+import { ifIphoneX } from 'react-native-iphone-x-helper';
+import { Post } from './components/Post';
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -23,9 +26,22 @@ export default class App extends Component<Props> {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
+        <Text style={[iOSUIKit.largeTitleEmphasized, styles.title]}>Random Saves</Text>
+        <View style={{flex:1}}>
+        <ScrollView 
+        showsVerticalScrollIndicator={true}
+        style={styles.scroll} >
+          <View style={styles.postContainer}>
+            <Post style={styles.post} title='Here 1' url='test'></Post>
+            <Post style={styles.post} title='Here 2' url='test2'></Post>
+            <Post style={styles.post} title='Here 3' url='test2'></Post>
+            <Post style={styles.post} title='Here 4' url='test2'></Post>
+            <Post style={styles.post} title='Here 5' url='test'></Post>
+            <Post style={styles.post} title='Here 6' url='test'></Post>
+            <Post style={styles.post} title='Here 7' url='test2'></Post>
+          </View>
+        </ScrollView>
+        </View>
       </View>
     );
   }
@@ -33,19 +49,31 @@ export default class App extends Component<Props> {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    ...ifIphoneX({
+      paddingTop: 50,
+    }, {
+      paddingTop: 20,
+    }),
     backgroundColor: '#F5FCFF',
+    flex: 1,
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+  scroll: {
+    height: '100%',
+    flex: 1,
   },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+  postContainer: {
+    padding: 20,
+    paddingTop: 5,
+    flex: 1,
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  title: {
+    marginLeft: 20,
+    textAlign: 'left',
+    alignSelf: 'stretch',
+  },
+  post: {
+    marginBottom: 20,
   },
 });
---
+++ components/__tests__/Hello.tsx
@@ -0,0 +1,10 @@
+// components/__tests__/Hello.tsx
+import React from 'react'
+import renderer from 'react-test-renderer'
+
+import { Hello } from "../Hello"
+
+it("renders correctly with defaults", () => {
+  const button = renderer.create(<Hello name="World" enthusiasmLevel={1} />).toJSON()
+  expect(button).toMatchSnapshot()
+})
